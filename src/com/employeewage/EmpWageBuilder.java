471f2d4a328a1bae22c316d772224b0c9e7affe7
ravi <ravi@gmail.com>
2021-10-20 18:12:49 +0530

    Done uc1 employee presence or absent

--- src/com/employeewage/EmpWageBuilder.java
+++ src/com/employeewage/EmpWageBuilder.java
@@ -5,6 +5,17 @@ package com.employeewage;
 public class EmpWageBuilder {
 
 	public static void main(String args[]) {
-		System.out.println("welcome employee wage builder programme");
+
+		// constant
+		int is_full_time = 1;
+
+		// Generate random value to check employee present or absent
+		double empcheck = Math.floor(Math.random() * 10) % 2;
+
+		// Condition statement
+		if (is_full_time == empcheck)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
 	}
 }
