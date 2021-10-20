3c98eb5bc99b4f7e01a58d4f717fa31263ac4947
ravi <ravi@gmail.com>
2021-10-20 19:07:51 +0530

    code for save total wages

--- src/com/employeewage/EmpWageBuilder.java
+++ src/com/employeewage/EmpWageBuilder.java
@@ -3,49 +3,69 @@ package com.employeewage;
 //EmpLoyee Wage Builder
 
 public class EmpWageBuilder {
-	//constant
-	public static final int IS_FULL_TIME = 1;
-	public static final int IS_PART_TIME = 2;
-	
+    //main method
+    public static void main(String args[]){
+        ComputeWageForEachCompany dmart = new ComputeWageForEachCompany("dMART", 20, 2, 10);
+	ComputeWageForEachCompany relaince = new ComputeWageForEachCompany("Relaince", 50, 10, 20);
+        dmart.computeWage();
+        System.out.println(dmart);
+        relaince.computeWage();
+        System.out.println(relaince);
+    }
+}
 
-	//main method
-	public static void main(String args[]){
-		computeEmpWage("DMART",20,2,10);		
-	}
-	
+class ComputeWageForEachCompany{
+    
+    //constant
+    public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
 
-	//Wage Computation method
-	public static int computeEmpWage(String company, int empRatePerHr, int numOfWorkingDays, int maxHrsPerMonth) {
+    private final String company;
+    private final int empRatePerHr;
+    private final int numOfWorkingDays;
+    private final int maxHrsPerMonth;
+    private int totalEmpWage;
 
-		//variables
-		int empHours = 0;
-		int totalWorkingDay = 0;
-		int totalEmpHours = 0;
+    //Wage Computation method
+    ComputeWageForEachCompany(String company, int empRatePerHr, int numOfWorkingDays, int maxHrsPerMonth) {
+        this.company = company;
+        this.empRatePerHr = empRatePerHr;
+        this.numOfWorkingDays = numOfWorkingDays;
+        this.maxHrsPerMonth = maxHrsPerMonth;
+    }
 
-		while (totalEmpHours <= maxHrsPerMonth && totalWorkingDay < numOfWorkingDays){
-			totalWorkingDay++;
-			//Generate random value to check employee do work full time, part time or absent
-			int empcheck = (int)Math.floor(Math.random()*10) % 3;
+    public void computeWage(){
+        //variables
+        int empHours = 0;
+        int totalWorkingDay = 0;
+        int totalEmpHours = 0;
 
-			//Condition statement switch case
-			switch(empcheck){
-				case IS_FULL_TIME:
-					empHours = 8;
-					break;
-				case IS_PART_TIME:
-					empHours = 4;
-					break;
-				default:
-					empHours = 0;
-			}
+        while (totalEmpHours <= maxHrsPerMonth && totalWorkingDay < numOfWorkingDays){
+                totalWorkingDay++;
+                //Generate random value to check employee do work full time, part time or absent
+                int empcheck = (int)Math.floor(Math.random()*10) % 3;
 
-			//calculate hours
-			totalEmpHours += empHours;
-			System.out.println("Working day : " + totalWorkingDay + " and Employee Hours : " + empHours);
-		}
+                //Condition statement switch case
+                switch(empcheck){
+                        case IS_FULL_TIME:
+                                empHours = 8;
+                                break;
+                        case IS_PART_TIME:
+                                empHours = 4;
+                                break;
+                        default:
+                                empHours = 0;
+                }
 
-		int totalEmpWage = totalEmpHours * empRatePerHr;
-		System.out.println("Monthly Employee Wage is " + totalEmpWage);
-		return totalEmpWage;
-	}
+                //calculate hours
+                totalEmpHours += empHours;
+                System.out.println("Working day : " + totalWorkingDay + " and Employee Hours : " + empHours);
+        }
+
+        totalEmpWage = totalEmpHours * empRatePerHr;
+    }
+    @Override
+    public String toString(){
+        return "Total Emp Wage for company "+ company + " is "+ totalEmpWage;
+    }
 }
