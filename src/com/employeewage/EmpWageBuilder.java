3b33b9c65d72660b793f96e8e4da135ea9b5466d
ravi <ravi@gmail.com>
2021-10-20 18:55:20 +0530

    code for calculate wages

--- src/com/employeewage/EmpWageBuilder.java
+++ src/com/employeewage/EmpWageBuilder.java
@@ -1,42 +1,48 @@
 package com.employeewage;
 
 //EmpLoyee Wage Builder
+
 public class EmpWageBuilder {
-	//constant
+	// constant
 	public static final int is_full_time = 1;
 	public static final int is_part_time = 2;
 	public static final int empRatePerHr = 20;
-	public static final int numOfWorkingday = 2;
+	public static final int numOfWorkingday = 20;
+	public static final int maxHrInMonth = 100;
 
-	//main method
+	// main method
 	public static void main(String args[]) {
 
-		//variables
+		// variables
 		int empHours = 0;
 		int empWage = 0;
-		int totalEmpWage = 0;
+		int totalWorkingDay = 0;
+		int totalEmpHours = 0;
 
-		for (int i = 0; i < numOfWorkingDay; i++){
-			//Generate random value to check employee do work full time, part time or absent
-			double empcheck = Math.floor(Math.random()*10) % 3;
+		while (totalEmpHours <= maxHrInMonth && totalWorkingDay <= numOfWorkingDay) {
+			totalWorkingDay++;
+			// Generate random value to check employee do work full time, part time or
+			// absent
+			int empcheck = (int) Math.floor(Math.random() * 10) % 3;
 
-			//Condition statement switch case
-			switch(empcheck){
-				case is_full_time:
-					empHours = 8;
-					break;
-				case is_part_time:
-					empHours = 4;
-					break;
-				default:
-					empHours = 0;
+			// Condition statement switch case
+			switch (empcheck) {
+			case is_full_time:
+				empHours = 8;
+				break;
+			case is_part_time:
+				empHours = 4;
+				break;
+			default:
+				empHours = 0;
 			}
 
-			//Calculat employee wage
-			empWage = empHours * empRatePerHr;
-			System.out,println("Employee Wage : " + empWage);
-			totalEmpWage += empWage;
+			// calculate hours
+			totalEmpHours += empHours;
+			System.out.println("Working day : " + totalWorkingDay + " and Employee Hours : " + empHours);
 		}
-		System.out.println("Monthly Wage Of Employee : " + totalEmpWage);
+
+		int totalEmpWage = totalEmpHours * empRatePerHr;
+		System.out.println("Monthly Employee Wage is " + totalEmpWage);
 	}
 }
