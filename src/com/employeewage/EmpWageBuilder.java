bf2f809ad28ea15b63eb5b4c84e2682801123095
ravi <ravi@gmail.com>
2021-10-20 18:03:18 +0530

    welcome to the employee wage builder

---
+++ src/com/employeewage/EmpWageBuilder.java
@@ -0,0 +1,10 @@
+package com.employeewage;
+
+//EmpLoyee Wage Builder
+
+public class EmpWageBuilder {
+
+	public static void main(String args[]) {
+		System.out.println("welcome employee wage builder programme");
+	}
+}
